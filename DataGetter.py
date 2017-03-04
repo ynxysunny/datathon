27105e631bfcc4c1c25111c56092c84386482d21
jeroen-chua <jeroen_chua@brown.edu>
2017-03-04 13:59:02 -0500

    ch

--- DataGetter.py
+++ DataGetter.py
@@ -1,27 +1,35 @@
 import numpy as np
 
-def get_train_data():
-    x_train_file = 'datathonData/UCI HAR Dataset/train/X_train.txt'
-    y_train_file = 'datathonData/UCI HAR Dataset/train/Y_train.txt'
-    subject_train_file = 'datathonData/UCI HAR Dataset/train/subject_train.txt'
+def get_data(is_train=True):
+    if is_train:
+        file_type = 'train'
+    else:
+        file_type = 'test'
+        
+    x_file = 'datathonData/UCI HAR Dataset/%s/X_%s.txt' %(file_type,file_type)
+    y_file = 'datathonData/UCI HAR Dataset/%s/Y_%s.txt' %(file_type,file_type)
+    subject_file = 'datathonData/UCI HAR Dataset/%s/subject_%s.txt' %(file_type,file_type)
     
-    x = np.loadtxt(x_train_file)
-    y = np.loadtxt(y_train_file)
-    subjects = np.loadtxt(subject_train_file)
-    
-    inertial_files = ['datathonData/UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt', \
-                      'datathonData/UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt', \
-                      'datathonData/UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt', \
-                      'datathonData/UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt', \
-                      'datathonData/UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt', \
-                      'datathonData/UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt', \
-                      'datathonData/UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt', \
-                      'datathonData/UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt', \
-                      'datathonData/UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt']
-    inerts = np.zeros((7352,128,len(inertial_files)))
+    x = np.loadtxt(x_file)
 
-    for i in range(0,len(inertial_files)):
-        tmp= np.loadtxt(inertial_files[i])
-        inerts[:,:,i]  = tmp
+    y = np.loadtxt(y_file)
+    subjects = np.loadtxt(subject_file)
     
-    return(x,y,subjects,inerts)
+    return (x,y,subjects)
+    
+#    inertial_files = ['datathonData/UCI HAR Dataset/%s/Inertial Signals/body_acc_x_%s.txt',\
+#                      'datathonData/UCI HAR Dataset/%s/Inertial Signals/body_acc_y_%s.txt',\
+#                      'datathonData/UCI HAR Dataset/%s/Inertial Signals/body_acc_z_%s.txt',\
+#                      'datathonData/UCI HAR Dataset/%s/Inertial Signals/body_gyro_x_%s.txt',\
+#                      'datathonData/UCI HAR Dataset/%s/Inertial Signals/body_gyro_y_%s.txt',\
+#                      'datathonData/UCI HAR Dataset/%s/Inertial Signals/body_gyro_z_%s.txt',\
+#                      'datathonData/UCI HAR Dataset/%s/Inertial Signals/total_acc_x_%s.txt',\
+#                      'datathonData/UCI HAR Dataset/%s/Inertial Signals/total_acc_y_%s.txt',\
+#                      'datathonData/UCI HAR Dataset/%s/Inertial Signals/total_acc_z_%s.txt']
+#    inerts = np.zeros((7352,128,len(inertial_files)))
+#
+#    for i in range(0,len(inertial_files)):
+#        tmp= np.loadtxt(inertial_files[i] %(file_type,file_type))
+#        inerts[:,:,i]  = tmp
+#    
+#    return(x,y,subjects,inerts)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import numpy as np
 import DataGetter
 
-(x,y,subjects,inerts) = DataGetter.get_train_data()
+(x_train,y_train,subjects_train) = DataGetter.get_data(is_train=True)
